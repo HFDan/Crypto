26426ad468f51b589e6701c15e489389c4f7d85f
HFDan <58553089+HFDan@users.noreply.github.com>
2021-03-22 20:38:23 +0200

    Argument Parsing

--- Crypto-CMake/src/Main.cpp
+++ Crypto-CMake/src/Main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <cryptopp.8.5.0/aes.h>
 
+enum class EEncType {
+
+	AES256
+
+};
+
+EEncType EncType = EEncType::AES256;
+
 int main(int argc, char* argv[]) {
 
+	for (int i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "-aes256") == 0) {
+			EncType = EEncType::AES256;
+		}
+		else {
+			exit(1);
+		}
+	}
 
 	exit(0);
 
